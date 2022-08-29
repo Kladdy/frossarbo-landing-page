b6c32d40a0bf6f99574d3059ea6201262398d173
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-08-29 10:02:47 +0200

    Add FA and i18n

--- scripts/build-scss.js
+++ scripts/build-scss.js
@@ -1,5 +1,18 @@
 'use strict';
+const upath = require('upath');
+const sh = require('shelljs');
 
 const renderSCSS = require('./render-scss');
 
 renderSCSS();
+
+// Copy over FontAwesome files
+const sourcePathFA = upath.resolve(upath.dirname(__filename), '../src/css/all.css');
+const destPathFA = upath.resolve(upath.dirname(__filename), '../dist/css/');
+
+sh.cp('-R', sourcePathFA, destPathFA)
+
+const sourcePathFAWebFonts = upath.resolve(upath.dirname(__filename), '../src/webfonts');
+const destPathFAWebFonts = upath.resolve(upath.dirname(__filename), '../dist/');
+
+sh.cp('-R', sourcePathFAWebFonts, destPathFAWebFonts) 
